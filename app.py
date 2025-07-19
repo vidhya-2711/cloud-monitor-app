aa0f86fc79d9f03d40e58073cbf38a488040e1fe
vidhya-2711 <vidhya2711@gmail.com>
2025-07-19 12:46:40 +0530

    🔄 Update dashboard UI and app logic for AWS Cloud Monitor

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def dashboard():
 
     return render_template("index.html", instances=instances, ecs_data=ecs_data, lambdas=lambdas, buckets=buckets)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0',port=5000 )
